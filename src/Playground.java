c3a0a5cbff8bb0482ea7fcbf0d64306993804dfd
1011 <woakswoaks@gmail.com>
2023-10-11 18:42:36 +0900

    2부 시작

---
+++ src/Playground.java
@@ -0,0 +1,11 @@
+public class Playground {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+
+            for (int j = 1; j < 8; j++) {
+                System.out.print(7 * i + j + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
