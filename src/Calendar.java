b887d2da30a291195f4691d1d0450976559ff140
1011 <woakswoaks@gmail.com>
2023-10-17 17:58:28 +0900

    요일 받아 출력

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,30 +1,42 @@
-import java.util.Scanner;
+
 
 public class Calendar {
 
-    final protected int[] maxDays = {31, 28,31,30,31,30,31,31,30,31,30,31};
+    final protected int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     public int getMaxDaysOfMonth(int month) {
 
-        return maxDays[month-1];
+        return maxDays[month - 1];
 
     }
 
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Calendar cal = new Calendar();
-
-        System.out.println("반복 횟수를 입력하세요");
-        int repeat = scan.nextInt();
-
-        for (int i = 0; i < repeat; i++) {
-            System.out.println("달을 입력하세요");
-            int month = scan.nextInt();
-            System.out.printf("%d월은 %d일까지 있습니다%n", month, cal.getMaxDaysOfMonth(month));
+    public void printCalendar(int year, int month, int firstDays) {
+        System.out.printf("   <<%d년, %d월 달력>>\n", year, month);
+        String[] days = new String[] {"일","월","화","수","목","금","토"};
+        for (int k = 0; k < days.length; k++) {
+            System.out.printf("%3s", days[k]);
         }
-        System.out.println("끝");
-        scan.close();
-    }
+        System.out.println("");
+        System.out.println("---------------------");
+        int max = getMaxDaysOfMonth(month);
+        if(month == 2 && (year%4==0 && ( year%100 !=0 || year%400==0 )))
+        { max = 29;}
+        for (int i = 0; i < firstDays; i++) {
+            System.out.printf("   ");
 
+        }
+            for (int i = 1; i <= max; i++) {
+                 System.out.printf("%3d", i);
+                if ((i+firstDays) % 7 == 0) {
+                    System.out.println("");
+                }
+             }
+
+        System.out.println("");
+        System.out.println("");
+    }
 }
+
+
+
+
---
+++ src/Prompt.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Prompt {
+
+    public int parseFirstDays(String str_firstDays) {
+        if (str_firstDays.equals("일")) return 0;
+        else if (str_firstDays.equals("월")) return 1;
+        else if (str_firstDays.equals("화")) return 2;
+        else if (str_firstDays.equals("수")) return 3;
+        else if (str_firstDays.equals("목")) return 4;
+        else if (str_firstDays.equals("금")) return 5;
+        else if (str_firstDays.equals("토")) return 6;
+        else {
+            return 0;
+        }
+    }
+
+    public void runPrompt() {
+        Scanner scan = new Scanner(System.in);
+        Calendar cal = new Calendar();
+        int month = 1;
+        int year = 1;
+        int firstDays = 0;
+        while (true) {
+            System.out.println("년을 입력하세요");
+            System.out.println("Year> ");
+            year = scan.nextInt();
+            System.out.println("달을 입력하세요");
+            System.out.print("Month> ");
+            month = scan.nextInt();
+            System.out.println("첫번째 요일을 입력하세요. (일, 월, 화, 수, 목, 금, 토)");
+            System.out.print("days> ");
+            String str_firstDays = scan.next();
+            firstDays = parseFirstDays(str_firstDays);
+            if (month < 1 || month > 12) {
+                System.out.println("달을 잘못 입력하였습니다. 다시 입력해 주세요.");
+                continue;
+            } else if (year < 1) {
+                System.out.println("연도를 잘못 입력하였습니다. 작업을 끝냅니다");
+                break;
+            } else {
+                cal.printCalendar(year, month, firstDays);
+            }
+        }
+        System.out.println("have a nice day!");
+        scan.close();
+    }
+
+    public static void main(String[] args) {
+        Prompt p = new Prompt();
+        p.runPrompt();
+
+
+    }
+}
